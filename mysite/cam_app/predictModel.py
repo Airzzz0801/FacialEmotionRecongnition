0a98815fb3ff780a4f6269734a50a8f30d2051f9
taojiajian009 <Airzzz_java@users.noreply.github.com>
2022-04-24 20:04:23 +0800

    change model.h5 path

--- mysite/cam_app/predictModel.py
+++ mysite/cam_app/predictModel.py
@@ -3,6 +3,6 @@ import tensorflow as tf
 model=None
 def load_model():
     global model
-    model = tf.keras.models.load_model('E:\Online class\FacialEmotionRecongnition\mysite\cam_app\model.h5')
+    model = tf.keras.models.load_model('mysite/model.h5')
     print(("* Loading Keras model and Flask starting server..."
                     "please wait until server has fully started"))
